964ef55006401d7edf737edd8ebb9cf450c073f5
Parker <parkerlamarbrook@gmail.com>
2023-07-17 18:58:50 -0600

    moved files out of nested folder

---
+++ temp/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import { Header, Footer, Navigation, Projects } from './Components';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Navigation />
+      <Projects />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ temp/Components/Navigation/index.jsx
@@ -0,0 +1,10 @@
+import { NavBarContainer, NavBar } from './style';
+import React from 'react';
+
+export const Navigation = () => {
+    return (
+        <NavBarContainer>
+            <NavBar>Navigation</NavBar>
+        </NavBarContainer>
+    )
+}
