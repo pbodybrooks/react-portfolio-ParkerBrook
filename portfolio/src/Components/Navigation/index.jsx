20b896fab95a3e23be774dfd8a773d371dedb432
Parker <parkerlamarbrook@gmail.com>
2023-07-17 18:44:06 -0600

    created basic skeleton for portfolio design

---
+++ portfolio/src/Components/Navigation/index.jsx
@@ -0,0 +1,15 @@
+import { NavBarContainer, NavButton } from './style';
+import React from 'react';
+
+export const Navigation = ({
+    onPageClick,
+    pages
+}) => {
+    return (
+        <NavBarContainer>
+            {pages.map(page => (
+                <NavButton onClick={() => onPageClick(page.key)}>{page.displayName}</NavButton>
+            ))}
+        </NavBarContainer>
+    )
+}
