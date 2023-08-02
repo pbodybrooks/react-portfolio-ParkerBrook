ae5042d8db12ce09f02d7afca901ceab59488e22
Parker <parkerlamarbrook@gmail.com>
2023-08-02 13:12:50 -0600

    reformatting with better reference material

--- src/App.js
+++ src/App.js
@@ -1,52 +1,6 @@
-import './App.css';
-import { Header, Footer, Navigation, Projects } from './Components';
-import { useState } from 'react';
+import React from "react";
+import PortfolioContainer from "./components/PortfolioContainer";
 
-function App() {
-  const pages = [
-    {
-      key: "about-me",
-      displayName: "About Me",
-    },
-    {
-      key: "projects",
-      displayName: "Projects",
-    },
-    {
-      key: "contact",
-      displayName: "Contact",
-    },
-    {
-      key: "resume",
-      displayName: "Resume",
-    },
-  ];
-
-  const [currentPage, setCurrentPage] = useState("about-me");
-
-  const renderPage = () => {
-    switch (currentPage) {
-      case "projects":
-        return <h1>Projects</h1>;
-      case "contact":
-        return <h1>Contact</h1>;
-      case "resume":
-        return <h1>Resume</h1>;
-      default:
-        return <h1>About Me</h1>;
-    }
-  };
-
-  return (
-    <div className="App">
-      <Header />
-      <Navigation pages={pages} onPageClick={setCurrentPage}/>
-      <div>{renderPage()}</div>
-      <Projects />
-      <Footer />
-    </div>
-  );
-}
+const App = () => <PortfolioContainer />;
 
 export default App;
-
